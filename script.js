7a21bd253b51dc4cc31e8bbd9374c808f58d5618
admin <admin@sohahib.mynetworksettings.com>
2022-11-21 01:39:28 -0500

    Reorganise

---
+++ script.js
@@ -0,0 +1,22 @@
+function moreOption(){
+        var x = document.getElementById("rules");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+
+      
+
+function toiletCleaningList(){
+    let names = ['Sohahib', 'Nassir', 'Faysal', 'Ousmane', 'Ibrahim', 'Tidiane']
+    let listTag = document.querySelector('ol');
+    let list = ``
+    names.forEach(name => {
+        list+= `<li>${name}</li>`
+    })
+    listTag.innerHTML=list;
+}
+
+toiletCleaningList()
